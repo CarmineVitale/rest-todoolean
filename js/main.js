f096e4c97163eeabf391322e6d980b88cd4cb0ca
CarmineVitale <carmine.vitale90@gmail.com>
2020-05-11 16:15:19 +0200

    Aggiunte funzioni e stile css

--- js/main.js
+++ js/main.js
@@ -10,49 +10,22 @@ $(document).ready(function () {
     print(apiUrl, template);
 
     btn.click(function() { 
-        var newTodo = input.val();
-        $.ajax({
-            url: apiUrl,
-            method: 'POST',
-            data: {
-                text: newTodo,
-            },
-            success: function() {
-                print(apiUrl, template);
-            },
-            error: function() {
-                alert('errore durante l0inserimento del nuovo todo item')
-            }
-        }) //fine chiamata ajax
+        creaTodo(apiUrl, template, input)
     }); //fine btn.click()
+    input.keyup(function(e) {
+        if (e.which == 13) {
+            creaTodo(apiUrl, template, input)
+        }
+    });
 
     $('body').on('click', '.delete', function() {
-        var item = $(this).data('id');
-        
-        $.ajax({
-            url: apiUrl + '/' + item,
-            method: 'DELETE',
-            success: function() {
-                print(apiUrl, template);
-            },
-            error: function() {
-                alert("Errore durante l'eliminazione di un todo item")
-            }
-
-        }); //fine chiamata ajax
-        
+        rimuoviTodo($(this), apiUrl,template);
     }); //fine click su span.delete
-    
 
-
-
-    
-
-
-    
 }); //<-- End ready
 
 //Funzioni 
+//STampa i todo
 function print(apiUrl, template) {
     $('.lista').html('');
 
@@ -76,3 +49,39 @@ function print(apiUrl, template) {
         }
     }); // Fine ajax
 } //Fine funzione print
+
+//crea i todo
+function creaTodo(apiUrl, template, input) {
+    var newTodo = input.val();
+    $.ajax({
+        url: apiUrl,
+        method: 'POST',
+        data: {
+            text: newTodo,
+        },
+        success: function() {
+            print(apiUrl, template);
+            
+        },
+        error: function() {
+            alert('errore durante l0inserimento del nuovo todo item')
+        }
+    }) //fine chiamata ajax
+    input.val('');
+} //Fine funzione crea todo
+
+//rimuovi todo
+function rimuoviTodo( self , apiUrl,template) { 
+    var item = self.data('id');
+        
+    $.ajax({
+        url: apiUrl + '/' + item,
+        method: 'DELETE',
+        success: function() {
+            print(apiUrl, template);
+        },
+        error: function() {
+            alert("Errore durante l'eliminazione di un todo item")
+        }
+    }); //fine chiamata ajax
+ } // fine funzione rimuovi todo
